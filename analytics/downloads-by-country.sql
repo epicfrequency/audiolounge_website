da505c65d34e909345a10eb8a6e4470eb7c89d7e
Chong Wang <24561914+epicfrequency@users.noreply.github.com>
2026-07-26 13:33:51 -0700

    Revert "1.04"
    This reverts commit e898630cfaaf57f772d0fd15c52056dac9909e33.

--- analytics/downloads-by-country.sql
+++
@@ -1,9 +0,0 @@
-SELECT
-  blob4 AS country,
-  sum(_sample_interval) AS download_clicks
-FROM audio_lounge_downloads
-WHERE
-  index1 = 'download'
-  AND timestamp >= NOW() - INTERVAL '30' DAY
-GROUP BY country
-ORDER BY download_clicks DESC;
--- analytics/downloads-by-day.sql
+++
@@ -1,9 +0,0 @@
-SELECT
-  toDate(timestamp) AS day,
-  sum(_sample_interval) AS download_clicks
-FROM audio_lounge_downloads
-WHERE
-  index1 = 'download'
-  AND timestamp >= NOW() - INTERVAL '30' DAY
-GROUP BY day
-ORDER BY day DESC;
